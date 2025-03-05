0fc1ef12bf370708f7c550c3b79a83a5f957e638
Ambroise Bareux <abareux@student.42mulhouse.fr>
2025-03-05 17:08:11 +0100

    tested with sfml for now

--- DisplayKey.cpp
+++ DisplayKey.cpp
@@ -1,70 +1,113 @@
 #include <iostream>
+#include <unordered_map>
 
+#include <SFML/Window.hpp>
 #include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 
-void displayKeyPressed(int key, sf::RenderWindow &window, sf::Sprite &sprite) {
-    float x = 0;
-    float y = 0;
+// Define a map of key positions (QWERTY layout example)
+#include <SFML/Graphics.hpp>
+#include <unordered_map>
 
-    switch (key) {
-//        case 0x30:  x = 500;    y = 75;     break;
-//        case 0x31:  x = 50;     y = 75;     break;
-//        case 0x32:  x = 100;    y = 75;     break;
-//        case 0x33:  x = 150;    y = 75;     break;
-//        case 0x34:  x = 200;    y = 75;     break;
-//        case 0x35:  x = 250;    y = 75;     break;
-//        case 0x36:  x = 300;    y = 75;     break;
-//        case 0x37:  x = 350;    y = 75;     break;
-//        case 0x38:  x = 400;    y = 75;     break;
-//        case 0x39:  x = 450;    y = 75;     break;
-        case 0x41: std::cout << "Key: A" << std::endl; break;
-        case 0x42: std::cout << "Key: B" << std::endl; break;
-        case 0x43: std::cout << "Key: C" << std::endl; break;
-        case 0x44: std::cout << "Key: D" << std::endl; break;
-        case 0x45: std::cout << "Key: E" << std::endl; break;
-        case 0x46: std::cout << "Key: F" << std::endl; break;
-        case 0x47: std::cout << "Key: G" << std::endl; break;
-        case 0x48: std::cout << "Key: H" << std::endl; break;
-        case 0x49: std::cout << "Key: I" << std::endl; break;
-        case 0x4A: std::cout << "Key: J" << std::endl; break;
-        case 0x4B: std::cout << "Key: K" << std::endl; break;
-        case 0x4C: std::cout << "Key: L" << std::endl; break;
-        case 0x4D: std::cout << "Key: M" << std::endl; break;
-        case 0x4E: std::cout << "Key: N" << std::endl; break;
-        case 0x4F: std::cout << "Key: O" << std::endl; break;
-        case 0x50: std::cout << "Key: P" << std::endl; break;
-        case 0x51: std::cout << "Key: Q" << std::endl; break;
-        case 0x52: std::cout << "Key: R" << std::endl; break;
-        case 0x53: std::cout << "Key: S" << std::endl; break;
-        case 0x54: std::cout << "Key: T" << std::endl; break;
-        case 0x55: std::cout << "Key: U" << std::endl; break;
-        case 0x56: std::cout << "Key: V" << std::endl; break;
-        case 0x57: std::cout << "Key: W" << std::endl; break;
-        case 0x58: std::cout << "Key: X" << std::endl; break;
-        case 0x59: std::cout << "Key: Y" << std::endl; break;
-        case 0x5A: std::cout << "Key: Z" << std::endl; break;
-        case 0x20: std::cout << "Key: Space" << std::endl; break;
-        case 0x0D: std::cout << "Key: Enter" << std::endl; break;
-        case 0x08: std::cout << "Key: Backspace" << std::endl; break;
-        case 0x1B: std::cout << "Key: Escape" << std::endl; break;
-        case 0x09: std::cout << "Key: Tab" << std::endl; break;
-        case 0x10: std::cout << "Key: Shift" << std::endl; break;
-        case 0x11: std::cout << "Key: Control" << std::endl; break;
-        case 0x12: std::cout << "Key: Alt" << std::endl; break;
-        case 0x1C: std::cout << "Key: Enter (Numpad)" << std::endl; break;
-        case 0x27: std::cout << "Key: Arrow Right" << std::endl; break;
-        case 0x25: std::cout << "Key: Arrow Left" << std::endl; break;
-        case 0x28: std::cout << "Key: Arrow Down" << std::endl; break;
-        case 0x26: std::cout << "Key: Arrow Up" << std::endl; break;
-        default:
-            break;
-    }
+// Define a map of key positions (QWERTY layout example)
+std::unordered_map<sf::Keyboard::Key, sf::Vector2f> keyPositions = {
+    // Row 1 (Numbers)
+    {sf::Keyboard::Key::Num1, {50, 75}}, {sf::Keyboard::Key::Num2, {100, 75}}, {sf::Keyboard::Key::Num3, {150, 75}},
+    {sf::Keyboard::Key::Num4, {200, 75}}, {sf::Keyboard::Key::Num5, {250, 75}}, {sf::Keyboard::Key::Num6, {300, 75}},
+    {sf::Keyboard::Key::Num7, {350, 75}}, {sf::Keyboard::Key::Num8, {400, 75}}, {sf::Keyboard::Key::Num9, {450, 75}},
+    {sf::Keyboard::Key::Num0, {500, 75}},
+
+    // Row 2 (QWERTY)
+    {sf::Keyboard::Key::Q, {75, 125}}, {sf::Keyboard::Key::W, {125, 125}}, {sf::Keyboard::Key::E, {175, 125}},
+    {sf::Keyboard::Key::R, {225, 125}}, {sf::Keyboard::Key::T, {275, 125}}, {sf::Keyboard::Key::Y, {325, 125}},
+    {sf::Keyboard::Key::U, {375, 125}}, {sf::Keyboard::Key::I, {425, 125}}, {sf::Keyboard::Key::O, {475, 125}},
+    {sf::Keyboard::Key::P, {525, 125}},
+
+    // Row 3 (ASDFG)
+    {sf::Keyboard::Key::A, {100, 175}}, {sf::Keyboard::Key::S, {150, 175}}, {sf::Keyboard::Key::D, {200, 175}},
+    {sf::Keyboard::Key::F, {250, 175}}, {sf::Keyboard::Key::G, {300, 175}}, {sf::Keyboard::Key::H, {350, 175}},
+    {sf::Keyboard::Key::J, {400, 175}}, {sf::Keyboard::Key::K, {450, 175}}, {sf::Keyboard::Key::L, {500, 175}},
+
+    // Row 4 (ZXCV)
+    {sf::Keyboard::Key::Z, {100, 225}}, {sf::Keyboard::Key::X, {150, 225}}, {sf::Keyboard::Key::C, {200, 225}},
+    {sf::Keyboard::Key::V, {250, 225}}, {sf::Keyboard::Key::B, {300, 225}}, {sf::Keyboard::Key::N, {350, 225}},
+    {sf::Keyboard::Key::M, {400, 225}},
+
+    // Special Keys
+    {sf::Keyboard::Key::Escape, {0, 0}}, {sf::Keyboard::Key::Enter, {650, 175}}, {sf::Keyboard::Key::Backspace, {650, 50}},
+    {sf::Keyboard::Key::Tab, {0, 125}}, {sf::Keyboard::Key::Space, {175, 275}}, {sf::Keyboard::Key::LShift, {0, 225}},
+    {sf::Keyboard::Key::RShift, {600, 225}}, {sf::Keyboard::Key::LControl, {0, 275}}, {sf::Keyboard::Key::RControl, {675, 275}},
+    {sf::Keyboard::Key::LAlt, {125, 275}}, {sf::Keyboard::Key::RAlt, {575, 275}}, {sf::Keyboard::Key::LSystem, {75, 275}},
+    {sf::Keyboard::Key::RSystem, {625, 275}}, {sf::Keyboard::Key::Menu, {650, 250}}, {sf::Keyboard::Key::PageUp, {700, 100}},
+    {sf::Keyboard::Key::PageDown, {700, 150}}, {sf::Keyboard::Key::End, {750, 150}}, {sf::Keyboard::Key::Home, {750, 100}},
+
+    // Arrow Keys
+    {sf::Keyboard::Key::Left, {750, 275}}, {sf::Keyboard::Key::Right, {850, 275}},
+    {sf::Keyboard::Key::Up, {800, 225}}, {sf::Keyboard::Key::Down, {800, 275}},
+
+    // Function Keys (F1 - F12)
+    {sf::Keyboard::Key::F1, {75, 0}}, {sf::Keyboard::Key::F2, {125, 0}}, {sf::Keyboard::Key::F3, {175, 0}},
+    {sf::Keyboard::Key::F4, {225, 0}}, {sf::Keyboard::Key::F5, {275, 0}}, {sf::Keyboard::Key::F6, {325, 0}},
+    {sf::Keyboard::Key::F7, {375, 0}}, {sf::Keyboard::Key::F8, {425, 0}}, {sf::Keyboard::Key::F9, {475, 0}},
+    {sf::Keyboard::Key::F10, {525, 0}}, {sf::Keyboard::Key::F11, {575, 0}}, {sf::Keyboard::Key::F12, {625, 0}},
+
+    // Numpad Keys
+    {sf::Keyboard::Key::Numpad0, {925, 275}}, {sf::Keyboard::Key::Numpad1, {925, 225}}, {sf::Keyboard::Key::Numpad2, {975, 225}},
+    {sf::Keyboard::Key::Numpad3, {1025, 225}}, {sf::Keyboard::Key::Numpad4, {925, 175}}, {sf::Keyboard::Key::Numpad5, {975, 175}},
+    {sf::Keyboard::Key::Numpad6, {1025, 175}}, {sf::Keyboard::Key::Numpad7, {925, 125}}, {sf::Keyboard::Key::Numpad8, {975, 125}},
+    {sf::Keyboard::Key::Numpad9, {1025, 125}},
+
+    // Punctuation & Symbols
+    {sf::Keyboard::Key::LBracket, {575, 125}}, {sf::Keyboard::Key::RBracket, {625, 125}}, {sf::Keyboard::Key::Semicolon, {550, 175}},
+    {sf::Keyboard::Key::Comma, {450, 225}}, {sf::Keyboard::Key::Period, {500, 225}}, {sf::Keyboard::Key::Apostrophe, {600, 175}},
+    {sf::Keyboard::Key::Slash, {550, 225}}, {sf::Keyboard::Key::Backslash, {675, 125}}, {sf::Keyboard::Key::Grave, {0, 75}},
+    {sf::Keyboard::Key::Equal, {600, 75}}, {sf::Keyboard::Key::Hyphen, {550, 75}}, {sf::Keyboard::Key::Add, {900, 250}},
+    {sf::Keyboard::Key::Subtract, {850, 225}}, {sf::Keyboard::Key::Multiply, {800, 225}}, {sf::Keyboard::Key::Divide, {750, 225}},
+
+    // Extra Keys
+    {sf::Keyboard::Key::Insert, {800, 125}}, {sf::Keyboard::Key::Delete, {800, 150}}, {sf::Keyboard::Key::Pause, {850, 50}}
+};
+
+bool keyPressed[100] = {false};
+
+void displayKeyPressed(const sf::Keyboard::Key key, sf::RenderWindow &window) {
+    if (key == sf::Keyboard::Key::Unknown || keyPressed[static_cast<int>(key)])
+        return;
+
+    sf::Vector2f position = keyPositions[key];
+
+    const sf::Image key_image(sf::Vector2u(50, 50), sf::Color(100, 100, 100));
+    const sf::Texture key_texture(key_image);
+    sf::Sprite key_sprite(key_texture);
+
+    key_sprite.setPosition(position);
+
+    keyPressed[static_cast<int>(key)] = true;
+
+    window.draw(key_sprite);
+    window.display();
+
+    std::cout << position.x << ", " << position.y << " pressed" << std::endl;
 }
 
-void displayKeyReleased(int key, sf::RenderWindow &window, sf::Sprite &sprite) {
-    std::cout << "key released" << std::endl;
-    return;
+void displayKeyReleased(const sf::Keyboard::Key key, sf::RenderWindow &window) {
+    if (key == sf::Keyboard::Key::Unknown || !keyPressed[static_cast<int>(key)])
+        return;
+
+    sf::Vector2f position = keyPositions[key];
+
+    const sf::Image key_image(sf::Vector2u(50, 50), sf::Color(50, 50, 50));
+    const sf::Texture key_texture(key_image);
+    sf::Sprite key_sprite(key_texture);
+
+    key_sprite.setPosition(position);
+
+    keyPressed[static_cast<int>(key)] = false;
+
+    window.draw(key_sprite);
+    window.display();
+
+    std::cout << position.x << ", " << position.y << " released" << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
 
-#include <vector>
-
 #include <SFML/Window.hpp>
 #include <SFML/Graphics/Image.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 
-#include <Windows.h>
+void displayKeyPressed(sf::Keyboard::Key key, sf::RenderWindow &window);
+void displayKeyReleased(sf::Keyboard::Key key, sf::RenderWindow &window);
 
-void displayKeyPressed(int key, sf::RenderWindow &window, sf::Sprite &sprite);
-void displayKeyReleased(int key, sf::RenderWindow &window, sf::Sprite &sprite);
-
-int main(int argc) {
+int main(const int argc, [[maybe_unused]] char **argv) {
 	if (argc != 1)
 		std::cout << "invalid use there is no argument" << std::endl;
 
-    int offset = 25;
+	constexpr int offset = 25;
 
-    unsigned int width = 50 * 21 + 2 * offset;
-    unsigned int height = 50 * 6 + 1 * offset;
+    constexpr unsigned int width = 50 * 21 + 3 * offset;
+    constexpr unsigned int height = 50 * 6 + 1 * offset;
 
 	sf::RenderWindow window(sf::VideoMode({width, height}), "Keyboard overlay");
 
-    sf::Image background_image(sf::Vector2u(width, height), sf::Color(50, 50, 50));
-    sf::Texture background_texture(background_image);
-    sf::Sprite background_sprite(background_texture);
+    const sf::Image background_image(sf::Vector2u(width, height), sf::Color(50, 50, 50));
+    const sf::Texture background_texture(background_image);
+    const sf::Sprite background_sprite(background_texture);
 
     window.draw(background_sprite);
     window.display();
@@ -34,44 +30,14 @@ int main(int argc) {
 
 	window.setFramerateLimit(60);
 
-    bool keys[0xFF] = {false};
-
-    std::vector<sf::Image> key_images;
-    std::vector<sf::Texture> key_textures;
-    std::vector<sf::Sprite> key_sprites;
-
-    sf::Color released_color;
-    sf::Vector2u key_dimension({50u, 50u});
-
-    for (int key = 0; key < 0xFF; key++) {
-        key_images.emplace_back(key_dimension, released_color);
-        key_textures.emplace_back(key_images[key]);
-        key_sprites.emplace_back(key_textures[key]);
-    }
-    window.draw(key_sprites[49]);
-
-    std::cout << key_images.size() << std::endl;
-    std::cout << key_textures.size() << std::endl;
-    std::cout << key_sprites.size() << std::endl;
-
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>())
 				window.close();
-
-            for (int key = 0; key < 0xFF; key++) {
-                SHORT keyState = GetAsyncKeyState(key);
-
-                if (keyState & 0x8000 && !keys[key]) {
-                    displayKeyPressed(key, window, key_sprites[key]);
-                    window.display();
-                    keys[key] = true;
-                } else if ((keyState & 0x8000) == 0 && keys[key]) {
-                    displayKeyReleased(key, window, key_sprites[key]);
-                    window.display();
-                    keys[key] = false;
-                }
-            }
+			if (event->is<sf::Event::KeyPressed>())
+				displayKeyPressed(event->getIf<sf::Event::KeyPressed>()->code, window);
+			if (event->is<sf::Event::KeyReleased>())
+				displayKeyReleased(event->getIf<sf::Event::KeyReleased>()->code, window);
 		}
 	}
 
