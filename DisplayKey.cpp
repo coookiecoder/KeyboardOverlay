87271076f790adb28fd490cb0196c11da5629cc4
abareux <abareux@student.42mulhouse.fr>
2025-03-05 18:22:11 +0100

    the app works using libuiohook

--- DisplayKey.cpp
+++ DisplayKey.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <libudev.h>
 #include <unordered_map>
 
 #include <SFML/Window.hpp>
@@ -7,76 +8,71 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 
-// Define a map of key positions (QWERTY layout example)
-#include <SFML/Graphics.hpp>
 #include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <uiohook.h>
 
 // Define a map of key positions (QWERTY layout example)
-std::unordered_map<sf::Keyboard::Key, sf::Vector2f> keyPositions = {
+std::unordered_map<uint16_t, sf::Vector2f> keyPositions = {
     // Row 1 (Numbers)
-    {sf::Keyboard::Key::Num1, {50, 75}}, {sf::Keyboard::Key::Num2, {100, 75}}, {sf::Keyboard::Key::Num3, {150, 75}},
-    {sf::Keyboard::Key::Num4, {200, 75}}, {sf::Keyboard::Key::Num5, {250, 75}}, {sf::Keyboard::Key::Num6, {300, 75}},
-    {sf::Keyboard::Key::Num7, {350, 75}}, {sf::Keyboard::Key::Num8, {400, 75}}, {sf::Keyboard::Key::Num9, {450, 75}},
-    {sf::Keyboard::Key::Num0, {500, 75}},
+    {VC_1, {50, 75}}, {VC_2, {100, 75}}, {VC_3, {150, 75}},
+    {VC_4, {200, 75}}, {VC_5, {250, 75}}, {VC_6, {300, 75}},
+    {VC_7, {350, 75}}, {VC_8, {400, 75}}, {VC_9, {450, 75}},
+    {VC_0, {500, 75}},
 
     // Row 2 (QWERTY)
-    {sf::Keyboard::Key::Q, {75, 125}}, {sf::Keyboard::Key::W, {125, 125}}, {sf::Keyboard::Key::E, {175, 125}},
-    {sf::Keyboard::Key::R, {225, 125}}, {sf::Keyboard::Key::T, {275, 125}}, {sf::Keyboard::Key::Y, {325, 125}},
-    {sf::Keyboard::Key::U, {375, 125}}, {sf::Keyboard::Key::I, {425, 125}}, {sf::Keyboard::Key::O, {475, 125}},
-    {sf::Keyboard::Key::P, {525, 125}},
+    {VC_Q, {75, 125}}, {VC_W, {125, 125}}, {VC_E, {175, 125}},
+    {VC_R, {225, 125}}, {VC_T, {275, 125}}, {VC_Y, {325, 125}},
+    {VC_U, {375, 125}}, {VC_I, {425, 125}}, {VC_O, {475, 125}},
+    {VC_P, {525, 125}},
 
     // Row 3 (ASDFG)
-    {sf::Keyboard::Key::A, {100, 175}}, {sf::Keyboard::Key::S, {150, 175}}, {sf::Keyboard::Key::D, {200, 175}},
-    {sf::Keyboard::Key::F, {250, 175}}, {sf::Keyboard::Key::G, {300, 175}}, {sf::Keyboard::Key::H, {350, 175}},
-    {sf::Keyboard::Key::J, {400, 175}}, {sf::Keyboard::Key::K, {450, 175}}, {sf::Keyboard::Key::L, {500, 175}},
+    {VC_A, {100, 175}}, {VC_S, {150, 175}}, {VC_D, {200, 175}},
+    {VC_F, {250, 175}}, {VC_G, {300, 175}}, {VC_H, {350, 175}},
+    {VC_J, {400, 175}}, {VC_K, {450, 175}}, {VC_L, {500, 175}},
 
     // Row 4 (ZXCV)
-    {sf::Keyboard::Key::Z, {100, 225}}, {sf::Keyboard::Key::X, {150, 225}}, {sf::Keyboard::Key::C, {200, 225}},
-    {sf::Keyboard::Key::V, {250, 225}}, {sf::Keyboard::Key::B, {300, 225}}, {sf::Keyboard::Key::N, {350, 225}},
-    {sf::Keyboard::Key::M, {400, 225}},
+    {VC_Z, {100, 225}}, {VC_X, {150, 225}}, {VC_C, {200, 225}},
+    {VC_V, {250, 225}}, {VC_B, {300, 225}}, {VC_N, {350, 225}},
+    {VC_M, {400, 225}},
 
     // Special Keys
-    {sf::Keyboard::Key::Escape, {0, 0}}, {sf::Keyboard::Key::Enter, {650, 175}}, {sf::Keyboard::Key::Backspace, {650, 50}},
-    {sf::Keyboard::Key::Tab, {0, 125}}, {sf::Keyboard::Key::Space, {175, 275}}, {sf::Keyboard::Key::LShift, {0, 225}},
-    {sf::Keyboard::Key::RShift, {600, 225}}, {sf::Keyboard::Key::LControl, {0, 275}}, {sf::Keyboard::Key::RControl, {675, 275}},
-    {sf::Keyboard::Key::LAlt, {125, 275}}, {sf::Keyboard::Key::RAlt, {575, 275}}, {sf::Keyboard::Key::LSystem, {75, 275}},
-    {sf::Keyboard::Key::RSystem, {625, 275}}, {sf::Keyboard::Key::Menu, {650, 250}}, {sf::Keyboard::Key::PageUp, {700, 100}},
-    {sf::Keyboard::Key::PageDown, {700, 150}}, {sf::Keyboard::Key::End, {750, 150}}, {sf::Keyboard::Key::Home, {750, 100}},
+    {VC_ESCAPE, {0, 0}}, {VC_ENTER, {650, 175}}, {VC_BACKSPACE, {650, 50}},
+    {VC_TAB, {0, 125}}, {VC_SPACE, {175, 275}}, {VC_SHIFT_L, {0, 225}},
+    {VC_SHIFT_R, {600, 225}}, {VC_CONTROL_L, {0, 275}}, {VC_CONTROL_R, {675, 275}},
+    {VC_ALT_L, {125, 275}}, {VC_ALT_R, {575, 275}}, {VC_META_L, {75, 275}},
+    {VC_META_R, {625, 275}}, {VC_CONTEXT_MENU, {650, 250}}, {VC_PAGE_UP, {700, 100}},
+    {VC_PAGE_DOWN, {700, 150}}, {VC_END, {750, 150}}, {VC_HOME, {750, 100}},
 
     // Arrow Keys
-    {sf::Keyboard::Key::Left, {750, 275}}, {sf::Keyboard::Key::Right, {850, 275}},
-    {sf::Keyboard::Key::Up, {800, 225}}, {sf::Keyboard::Key::Down, {800, 275}},
+    {VC_LEFT, {750, 275}}, {VC_RIGHT, {850, 275}},
+    {VC_UP, {800, 225}}, {VC_DOWN, {800, 275}},
 
     // Function Keys (F1 - F12)
-    {sf::Keyboard::Key::F1, {75, 0}}, {sf::Keyboard::Key::F2, {125, 0}}, {sf::Keyboard::Key::F3, {175, 0}},
-    {sf::Keyboard::Key::F4, {225, 0}}, {sf::Keyboard::Key::F5, {275, 0}}, {sf::Keyboard::Key::F6, {325, 0}},
-    {sf::Keyboard::Key::F7, {375, 0}}, {sf::Keyboard::Key::F8, {425, 0}}, {sf::Keyboard::Key::F9, {475, 0}},
-    {sf::Keyboard::Key::F10, {525, 0}}, {sf::Keyboard::Key::F11, {575, 0}}, {sf::Keyboard::Key::F12, {625, 0}},
+    {VC_F1, {75, 0}}, {VC_F2, {125, 0}}, {VC_F3, {175, 0}},
+    {VC_F4, {225, 0}}, {VC_F5, {275, 0}}, {VC_F6, {325, 0}},
+    {VC_F7, {375, 0}}, {VC_F8, {425, 0}}, {VC_F9, {475, 0}},
+    {VC_F10, {525, 0}}, {VC_F11, {575, 0}}, {VC_F12, {625, 0}},
 
     // Numpad Keys
-    {sf::Keyboard::Key::Numpad0, {925, 275}}, {sf::Keyboard::Key::Numpad1, {925, 225}}, {sf::Keyboard::Key::Numpad2, {975, 225}},
-    {sf::Keyboard::Key::Numpad3, {1025, 225}}, {sf::Keyboard::Key::Numpad4, {925, 175}}, {sf::Keyboard::Key::Numpad5, {975, 175}},
-    {sf::Keyboard::Key::Numpad6, {1025, 175}}, {sf::Keyboard::Key::Numpad7, {925, 125}}, {sf::Keyboard::Key::Numpad8, {975, 125}},
-    {sf::Keyboard::Key::Numpad9, {1025, 125}},
+    {VC_KP_0, {925, 275}}, {VC_KP_1, {925, 225}}, {VC_KP_2, {975, 225}},
+    {VC_KP_3, {1025, 225}}, {VC_KP_4, {925, 175}}, {VC_KP_5, {975, 175}},
+    {VC_KP_6, {1025, 175}}, {VC_KP_7, {925, 125}}, {VC_KP_8, {975, 125}},
+    {VC_KP_9, {1025, 125}},
 
     // Punctuation & Symbols
-    {sf::Keyboard::Key::LBracket, {575, 125}}, {sf::Keyboard::Key::RBracket, {625, 125}}, {sf::Keyboard::Key::Semicolon, {550, 175}},
-    {sf::Keyboard::Key::Comma, {450, 225}}, {sf::Keyboard::Key::Period, {500, 225}}, {sf::Keyboard::Key::Apostrophe, {600, 175}},
-    {sf::Keyboard::Key::Slash, {550, 225}}, {sf::Keyboard::Key::Backslash, {675, 125}}, {sf::Keyboard::Key::Grave, {0, 75}},
-    {sf::Keyboard::Key::Equal, {600, 75}}, {sf::Keyboard::Key::Hyphen, {550, 75}}, {sf::Keyboard::Key::Add, {900, 250}},
-    {sf::Keyboard::Key::Subtract, {850, 225}}, {sf::Keyboard::Key::Multiply, {800, 225}}, {sf::Keyboard::Key::Divide, {750, 225}},
+    {VC_OPEN_BRACKET, {575, 125}}, {VC_CLOSE_BRACKET, {625, 125}}, {VC_SEMICOLON, {550, 175}},
+    {VC_COMMA, {450, 225}}, {VC_PERIOD, {500, 225}}, {VC_QUOTE, {600, 175}},
+    {VC_SLASH, {550, 225}}, {VC_BACKSPACE, {675, 125}}, {VC_EQUALS, {600, 75}},
+	{VC_MINUS, {850, 225}},
 
     // Extra Keys
-    {sf::Keyboard::Key::Insert, {800, 125}}, {sf::Keyboard::Key::Delete, {800, 150}}, {sf::Keyboard::Key::Pause, {850, 50}}
+    {VC_INSERT, {800, 125}}, {VC_DELETE, {800, 150}}, {VC_PAUSE, {850, 50}}
 };
 
-bool keyPressed[100] = {false};
-
-void displayKeyPressed(const sf::Keyboard::Key key, sf::RenderWindow &window) {
-    if (key == sf::Keyboard::Key::Unknown || keyPressed[static_cast<int>(key)])
-        return;
-
-    sf::Vector2f position = keyPositions[key];
+void displayKeyPressed(const keyboard_event_data event_data, sf::RenderWindow &window) {
+    sf::Vector2f position = keyPositions[event_data.keycode];
 
     const sf::Image key_image(sf::Vector2u(50, 50), sf::Color(100, 100, 100));
     const sf::Texture key_texture(key_image);
@@ -84,27 +80,20 @@ void displayKeyPressed(const sf::Keyboard::Key key, sf::RenderWindow &window) {
 
     key_sprite.setPosition(position);
 
-    keyPressed[static_cast<int>(key)] = true;
-
     window.draw(key_sprite);
     window.display();
 
     std::cout << position.x << ", " << position.y << " pressed" << std::endl;
 }
 
-void displayKeyReleased(const sf::Keyboard::Key key, sf::RenderWindow &window) {
-    if (key == sf::Keyboard::Key::Unknown || !keyPressed[static_cast<int>(key)])
-        return;
-
-    sf::Vector2f position = keyPositions[key];
+void displayKeyReleased(const keyboard_event_data event_data, sf::RenderWindow &window) {
+	sf::Vector2f position = keyPositions[event_data.keycode];
 
     const sf::Image key_image(sf::Vector2u(50, 50), sf::Color(50, 50, 50));
     const sf::Texture key_texture(key_image);
     sf::Sprite key_sprite(key_texture);
 
     key_sprite.setPosition(position);
-
-    keyPressed[static_cast<int>(key)] = false;
 
     window.draw(key_sprite);
     window.display();
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics/Image.hpp>
@@ -6,19 +7,28 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 
-void displayKeyPressed(sf::Keyboard::Key key, sf::RenderWindow &window);
-void displayKeyReleased(sf::Keyboard::Key key, sf::RenderWindow &window);
+#include <uiohook.h>
+
+void displayKeyPressed(keyboard_event_data event_data, sf::RenderWindow &window);
+void displayKeyReleased(keyboard_event_data event_data, sf::RenderWindow &window);
+
+constexpr int offset = 25;
+
+constexpr unsigned int width = 50 * 21 + 3 * offset;
+constexpr unsigned int height = 50 * 6 + 1 * offset;
+
+sf::RenderWindow window(sf::VideoMode({width, height}), "Keyboard overlay");
+
+void onKeyEvent(uiohook_event* event) {
+	if (event->type == EVENT_KEY_PRESSED)
+		displayKeyPressed(event->data.keyboard, window);
+	if (event->type == EVENT_KEY_RELEASED)
+		displayKeyReleased(event->data.keyboard, window);
+}
 
 int main(const int argc, [[maybe_unused]] char **argv) {
 	if (argc != 1)
 		std::cout << "invalid use there is no argument" << std::endl;
-
-	constexpr int offset = 25;
-
-    constexpr unsigned int width = 50 * 21 + 3 * offset;
-    constexpr unsigned int height = 50 * 6 + 1 * offset;
-
-	sf::RenderWindow window(sf::VideoMode({width, height}), "Keyboard overlay");
 
     const sf::Image background_image(sf::Vector2u(width, height), sf::Color(50, 50, 50));
     const sf::Texture background_texture(background_image);
@@ -27,20 +37,24 @@ int main(const int argc, [[maybe_unused]] char **argv) {
     window.draw(background_sprite);
     window.display();
     window.draw(background_sprite);
+	window.display();
 
 	window.setFramerateLimit(60);
+
+	hook_set_dispatch_proc(onKeyEvent);
+	if (hook_run() != UIOHOOK_SUCCESS) {
+		std::cout << "error" << std::endl;
+		window.close();
+	}
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>())
 				window.close();
-			if (event->is<sf::Event::KeyPressed>())
-				displayKeyPressed(event->getIf<sf::Event::KeyPressed>()->code, window);
-			if (event->is<sf::Event::KeyReleased>())
-				displayKeyReleased(event->getIf<sf::Event::KeyReleased>()->code, window);
 		}
 	}
 
+	hook_stop();
 	window.clear();
 	return (0);
 }
